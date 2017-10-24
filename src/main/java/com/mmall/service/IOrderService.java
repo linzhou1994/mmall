398ae02c16dff7a0905058eb543e7f0d9972bf0f
linzhou <970060731@qq.com>
2017-10-24 10:34:55 +0800

    完成支付宝模块功能

---
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -0,0 +1,52 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+
+import java.util.Map;
+
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ * 佛曰:
+ * 写字楼里写字间，写字间里程序员；
+ * 程序人员写程序，又拿程序换酒钱。
+ * 酒醒只在网上坐，酒醉还来网下眠；
+ * 酒醉酒醒日复日，网上网下年复年。
+ * 但愿老死电脑间，不愿鞠躬老板前；
+ * 奔驰宝马贵者趣，公交自行程序员。
+ * 别人笑我忒疯癫，我笑自己命太贱；
+ * 不见满街漂亮妹，哪个归得程序员？
+ * ---------------------------
+ * 项目名： mmall
+ * 包名：   com.mmall.service
+ * 创建者:  linzhou
+ * 创建时间:17/10/19
+ * 描述:
+ */
+public interface IOrderService {
+
+    ServerResponse pay(Long orderNo, Integer userId, String path);
+
+    ServerResponse aliCallback(Map<String,String> params);
+
+    ServerResponse queryOrderPayStatus(Integer userId,Long orderNo);
+}
