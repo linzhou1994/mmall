bbf7f1f55fb9563e50c708b9b0319ca0822f8c25
linzhou <970060731@qq.com>
2017-11-23 12:25:19 +0800

    完成支付宝模块功能

--- src/main/java/com/mmall/controller/portal/CartController.java
+++ src/main/java/com/mmall/controller/portal/CartController.java
@@ -68,8 +68,9 @@ public class CartController {
     @ResponseBody
     public ServerResponse<CartVo> list(HttpSession session){
         User user= (User) session.getAttribute(Const.CURRENT_USER);
-        if (user==null)
+        if (user==null){
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
 
         return mICartService.list(user.getId());
     }
